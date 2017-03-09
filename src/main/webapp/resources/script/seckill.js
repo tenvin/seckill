c93c8f36ed28f2c0531abe7b35bbde3f81ae8d41
tenvin <tenvin@gmail.com>
2017-03-09 16:58:59 +0800

    update

---
+++ src/main/webapp/resources/script/seckill.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 2017/3/9.
+ */
+var seckill = {
+    URL : {
+        now : function(){
+            return '/seckill/time/now';
+        }
+    },
+    validatePhone : function(phone){
+        if(phone && phone.length ==11 && isNaN(phone)){
+            return true;
+        }else{
+            return false;
+        }
+
+    },
+    detail : {
+        init : function(params){
+            var killPhone = $.cookie('killPhone');
+            var startTime = params['startTime'];
+            var endTime = params['endTime'];
+            var seckillId = params['seckillId'];
+
+            if(!seckill.validatePhone(killPhone)){
+                var killPhoneModal = $.('#killPhoneModal');
+
+            }
+        }
+    }
+}
